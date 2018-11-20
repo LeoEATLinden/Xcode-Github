2de09b9779f51524075947b9e57de633882b42bc
Leo Lindén <leolin@kth.se>
2018-11-20 21:53:20 +0000

    Initial Commit

---
+++ Xcode-Github/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Xcode-Github
+//
+//  Created by Leo Lindén on 2018-11-20.
+//  Copyright © 2018 Leo Lindén. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
